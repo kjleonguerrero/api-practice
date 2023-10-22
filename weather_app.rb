5944b2afd3f455da17106208c088e90aa8c5a303
kjleonguerrero <139664760+kjleonguerrero@users.noreply.github.com>
2023-10-22 18:44:45 +1000

    addinng loop till quit

--- weather_app.rb
+++ weather_app.rb
@@ -1,14 +1,24 @@
-require "HTTP"
+require "http"
 
 puts "Welcome to Kyle's Weather App!!"
 
-response = HTTP.get("https://api.openweathermap.org/data/2.5/weather?q=guam&units=imperial&appid=#{ENV["OPENN_WEATHER_API_KEY"]}")
+loop do
+  puts "Enter a city name (or 'quit' to exit):"
+  city = gets.chomp
 
-weatherdata = response.parse
+  break if city.downcase == "quit"
 
-temp = weatherdata["main"]["temp"]
-desc = weatherdata["weather"][0]["description"]
+  puts "Enter preferred temperature units (C for Celsius or F for Fahrenheit):"
+  units = gets.chomp.downcase
+  units = "imperial" if units == "F"
+  units = "metric" if units == "C"
 
-puts "Guam is #{temp} degrees outside with #{desc}"
+  response = HTTP.get("https://api.openweathermap.org/data/2.5/weather?q=#{city}&units=#{units}&appid=#{ENV["OPEN_WEATHER_API_KEY"]}")
 
-test
+  weatherdata = response.parse
+
+  temp = weatherdata["main"]["temp"]
+  desc = weatherdata["weather"][0]["description"]
+
+  puts "#{city} is #{temp} degrees outside with #{desc}"
+end
